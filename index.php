a57ae4a3df233614bbc0f783394ad9b364d2de12
extreemedev <mattallev@gmail.com>
2022-04-30 22:28:38 +0200

    matteo

--- index.php
+++ index.php
@@ -2,11 +2,49 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="icon" href="../bootstrap/assets/img/favicons/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SitoWeb</title>
+    <title>LunAPPark ~ Homepage</title>
 </head>
-<body>
+<body class="text-center">
+    <div class="mb-3">
+        <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    </div>
+    <h1>Benvenuti al LunAPPark</h1>
+    <div class="mb-3">
+    </div>
+  </div>
+  <h1>Scopri di piu</h1>
+  <div class="mb-3">
+  </div>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="gardaland.jpg" class="d-block w-100" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="bootstrap/assets/img/bootstrap-icons.png" class="d-block w-100" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="bootstrap/assets/img/examples/album-rtl.png" class="d-block w-100" alt="">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+    </div>
     
 </body>
 </html>
