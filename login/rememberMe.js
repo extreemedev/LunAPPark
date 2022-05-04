2e984914f63e97e940a734ab33972572e0819aa0
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-04 11:53:26 +0200

    francesco

---
+++ login/rememberMe.js
@@ -0,0 +1,8 @@
+function validaForm() {
+    if (document.myForm.remember.checked) {
+        window.alert("hai scelto di ricordarti di obama");
+    }
+    else {
+        window.alert("hai scelto di non ricordarti di obama");
+    }
+}
