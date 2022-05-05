d21bcade4601d10a4ceac4e9fa070f1bf1b4add1
extreemedev <mattallev@gmail.com>
2022-05-06 01:35:53 +0200

    matt

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta http-equiv="refresh" content="5" />
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./wondermaster/dist/css/styles.css">
     <link rel="icon" href="../bootstrap/assets/img/favicons/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LunAPPark ~ Homepage</title>
+    <title>LunAPPark Homepage</title>
 </head>
 <body class="text-center">
-    <div class="mb-3">
-        <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
-    </div>
-    <h1>Benvenuti al LunAPPark</h1>
-    <div class="mb-3">
-    </div>
-  </div>
-  <h1>Scopri di piu</h1>
-  <div class="mb-3">
-  </div>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top">LunAPPark</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>
+                        <li class="nav-item"><a class="nav-link" href="login/index.html" role="button">Log In</a></li>
+                    </ul>
+                </div>
+            </div>
+    </nav>  
+    <div class="mb-lg-5"></div>
+
 
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
@@ -27,7 +34,7 @@
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="gardaland.jpg" class="d-block w-100" alt="">
+            <img src="parchi-a-tema-00.jpg" class="d-block w-100" alt="">
           </div>
           <div class="carousel-item">
             <img src="bootstrap/assets/img/bootstrap-icons.png" class="d-block w-100" alt="">
@@ -44,7 +51,7 @@
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
-    </div>
-    
+    </div>	
+	
 </body>
 </html>
