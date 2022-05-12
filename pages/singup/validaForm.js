936864071bb74ec6cbea30a4985b2d500c2c06fe
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-12 17:35:50 +0200

    francesco

--- pages/singup/validaForm.js
+++ pages/singup/validaForm.js
@@ -1,8 +1,20 @@
-function validaForm() {
-    if (document.myForm.remember.checked) {
-        window.alert("Hai scelto di ricordare le credenziali");
-    }
-    else {
-        window.alert("Hai scelto di non ricordare le credenziali");
-    }
-}
+(function valida() {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+  
