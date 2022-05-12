c07a8e4a6c1a29279b605b4ff694c4a11cb912dd
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-12 17:22:32 +0200

    francesco

---
+++ pages/singup/validaForm.js
@@ -0,0 +1,8 @@
+function validaForm() {
+    if (document.myForm.remember.checked) {
+        window.alert("Hai scelto di ricordare le credenziali");
+    }
+    else {
+        window.alert("Hai scelto di non ricordare le credenziali");
+    }
+}
