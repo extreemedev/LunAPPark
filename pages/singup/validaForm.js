04681eb8c8367bc22719aa7347006c5d8ab63dd9
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-13 18:48:46 +0200

    francesco

--- pages/singup/validaForm.js
+++ pages/singup/validaForm.js
@@ -1,20 +1,34 @@
-(function valida() {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
-  
+function valida() {
+    if (document.myRegistr.cognome.value=="") {
+        window.alert("Inserire cognome");
+        return false;
+    }
+
+    if (document.myRegistr.nome.value=="") {
+        window.alert("Inserire nome");
+        return false;
+    }
+
+    //if (document.myRegistr.matricola.value=="") {
+     // windows.alert("Inserire matricola");
+    //  return false;
+   // }
+
+    //var v=parseInt(document.myRegistr.matricola.value);
+    //if (isNaN(v)) {
+    //  windows.alert("La matricola deve essere un numero");
+    //  return false;
+   // }
+
+   // if (document.myRegistr.regione.value=="nessuna") {
+    //  windows.alert("Selezionare una regione");
+    //  return false;
+   // }
+
+    if ((document.myRegistr.email.value=="")&&(document.myRegistr.tel.value=="")) {
+        window.alert("Inserire o l’email o il numero di telefono");
+        return false;
+    }
+    window.alert("Dati inseriti correttamente");
+    return true;
+}
