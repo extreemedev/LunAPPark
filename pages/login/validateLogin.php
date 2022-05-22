696a179f0939959a7fc0f3ee86819a6d2630bd6a
extreemedev <mattallev@gmail.com>
2022-05-22 23:36:24 +0200

    matt

--- pages/login/validateLogin.php
+++ pages/login/validateLogin.php
@@ -1,36 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        if(!isset($_POST["loginButton"])) {
-            header("Location: index.html");
+<?php
+    if(!isset($_POST["loginButton"])) {
+        header("Location: index.html");
+    }
+    else{
+        $dbconn = pg_connect("host=postgresql port=5432
+        dbname=lunappark user=extreemedev password=example");
+        $email = $_POST["inputEmail"];
+        $query = 'SELECT * FROM public."UTENTE" where email=$1';
+        $result = pg_query_params($dbconn, $query, array($email));
+        if(!($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
+            echo"L'utente non é registrato!";
         }
-        else{
-            $dbconn = pg_connect("host=postgresql port=5432
-            dbname=lunappark user=extreemedev password=example");
-            $email = $_POST["inputEmail"];
-            $query = 'SELECT * FROM public."UTENTE" where email=$1';
-            $result = pg_query_params($dbconn, $query, array($email));
-            if(!($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
-                echo"L'utente non é registrato!";
+        else {
+            $password = md5($_POST["inputPassword"]);
+            $query2 = 'SELECT * FROM public."UTENTE" where email=$1 and password=$2';
+            $result = pg_query_params($dbconn, $query2, array($email, $password));
+            if(($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
+                echo"Il login é andato a buon fine";
             }
-            else {
-                $password = md5($_POST["inputPassword"]);
-                $query2 = 'SELECT * FROM public."UTENTE" where email=$1 and password=$2';
-                $result = pg_query_params($dbconn, $query2, array($email, $password));
-                if(($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
-                    echo"Il login é andato a buon fine";
-                }
-                else die("Email o password errate!");
-            }
+            else die("Email o password errate!");
         }
-    ?>
-    
-</body>
-</html>
+    }
+?>
--- pages/signup/validateRegistration.php
+++ pages/signup/validateRegistration.php
@@ -1,43 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="validaForm.js"></script>
-</head>
-<body>
-    <?php
-        if(!isset($_POST["signupButton"])) {
-            header("Location: index.html");
+<?php
+    if(!isset($_POST["signupButton"])) {
+        header("Location: index.html");
+    }
+    else{
+        $dbconn = pg_connect("host=postgresql port=5432
+        dbname=lunappark user=extreemedev password=example");
+        $email = $_POST["email"];
+        $query = 'SELECT * FROM public."UTENTE" where email=$1';
+        $result = pg_query_params($dbconn, $query, array($email));
+        if($tuple=pg_fetch_array($result,null,PGSQL_ASSOC)) {
+            echo"La registrazione non e' andata a buon fine \n";
+            echo"Nel nostro sistema esiste già un account con questa email";
         }
-        else{
-            $dbconn = pg_connect("host=postgresql port=5432
-            dbname=lunappark user=extreemedev password=example");
-            $email = $_POST["email"];
-            $query = 'SELECT * FROM public."UTENTE" where email=$1';
-            $result = pg_query_params($dbconn, $query, array($email));
-            if($tuple=pg_fetch_array($result,null,PGSQL_ASSOC)) {
-                echo"La registrazione non e' andato a buon fine \n";
-                echo"Nel nostro sistema essiste già un account con questa email";
+        else {
+            $nome = $_POST["nome"];
+            $cognome = $_POST["cognome"];
+            $telefono = $_POST["tel"];
+            $datanasc = $_POST["data"];
+            $password = md5($_POST["password"]);
+            $query2 = 'INSERT into public."UTENTE" values ($1,$2,$3,$4,$5,$6)';
+            $result = pg_query_params($dbconn, $query2, array($email, $nome, $cognome, $password, $telefono, $datanasc));
+            if($result) {
+                echo'<script type="text/javascript">myFunction();</script>';
+                /*header("Location: ../login/index.html");*/
             }
-            else {
-                $nome = $_POST["nome"];
-                $cognome = $_POST["cognome"];
-                $telefono = $_POST["tel"];
-                $datanasc = $_POST["data"];
-                $password = md5($_POST["password"]);
-                $query2 = 'INSERT into public."UTENTE" values ($1,$2,$3,$4,$5,$6)';
-                $result = pg_query_params($dbconn, $query2, array($email, $nome, $cognome, $password, $telefono, $datanasc));
-                if($result) {
-                    echo'<script type="text/javascript">myFunction();</script>';
-                    header("Location: ../login/index.html");
-                }
-                else die("C'e' stato un errore");
-            }
+            else die("C'é stato un errore");
         }
-    ?>
-    
-</body>
-</html>
+    }
+?>
