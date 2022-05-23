beaaf4e33f5b2ac8609c1e5366e760e6b41c0555
extreemedev <mattallev@gmail.com>
2022-05-23 17:26:04 +0200

    matt

--- pages/login/validateLogin.php
+++ pages/login/validateLogin.php
@@ -1,24 +1,19 @@
 <?php
-    if(!isset($_POST["loginButton"])) {
-        header("Location: index.php");
+    $dbconn = pg_connect("host=postgresql port=5432
+    dbname=lunappark user=extreemedev password=example");
+    $email = $_POST["inputEmail"];
+    $query = 'SELECT * FROM public."UTENTE" where email=$1';
+    $result = pg_query_params($dbconn, $query, array($email));
+    if(!($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
+        echo"L'utente non é registrato!";
     }
-    else{
-        $dbconn = pg_connect("host=postgresql port=5432
-        dbname=lunappark user=extreemedev password=example");
-        $email = $_POST["inputEmail"];
-        $query = 'SELECT * FROM public."UTENTE" where email=$1';
-        $result = pg_query_params($dbconn, $query, array($email));
-        if(!($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
-            echo"L'utente non é registrato!";
+    else {
+        $password = md5($_POST["inputPassword"]);
+        $query2 = 'SELECT * FROM public."UTENTE" where email=$1 and password=$2';
+        $result = pg_query_params($dbconn, $query2, array($email, $password));
+        if(($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
+            header("location: yourarea.php");
         }
-        else {
-            $password = md5($_POST["inputPassword"]);
-            $query2 = 'SELECT * FROM public."UTENTE" where email=$1 and password=$2';
-            $result = pg_query_params($dbconn, $query2, array($email, $password));
-            if(($tuple=pg_fetch_array($result,null,PGSQL_ASSOC))) {
-                header("location: yourarea.php");
-            }
-            else die("Email o password errate!");
-        }
+        else die("Email o password errate!");
     }
 ?>
