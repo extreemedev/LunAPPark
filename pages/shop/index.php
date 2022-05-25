a64a802702c6cc48b1afa7302f8d5c6b95d7e866
extreemedev <mattallev@gmail.com>
2022-05-25 19:52:46 +0200

    shop

--- pages/shop/index.php
+++ pages/shop/index.php
@@ -4,9 +4,11 @@ define("TITLE","Shop");
 
 <?php
 include("../layouts/header.php")
-?>
-
-    <a class="logo" style="margin-left: auto;" href="#page-top"><img src="../../src/assets/img/icons8-cart-64.png" alt="logo"></a>
+?>  
+    <div class="pt-5"></div>
+    <div class="container" >
+    <a class="cart" href="#page-top"><img src="../../src/assets/img/icons8-cart-64.png" alt="cart"></a>
+    </div>
 <?php
 include("../layouts/footer.php")
 ?>
