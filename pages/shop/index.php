8c0a2418d9b1668e461293816d61a9f28fdce039
extreemedev <mattallev@gmail.com>
2022-05-25 22:01:17 +0200

    cart

--- pages/shop/index.php
+++ pages/shop/index.php
@@ -6,8 +6,8 @@ define("TITLE","Shop");
 include("../layouts/header.php")
 ?>  
     <div class="pt-5"></div>
-    <div class="container" >
-    <a class="cart" href="#page-top"><img src="../../src/assets/img/icons8-cart-64.png" alt="cart"></a>
+    <div class="container">
+    <a id="carrello"  class="cart" style="position: sticky; right: 200px;" href="#page-top"><img src="../../src/assets/img/icons8-cart-64.png" alt="cart"></a>
     </div>
 <?php
 include("../layouts/footer.php")
