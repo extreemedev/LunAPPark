4c389895db26a11f4098e8ccc360ab28793cef10
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-26 16:27:26 +0200

    calendario francesco

--- pages/calendar/calendar.php
+++ pages/calendar/calendar.php
@@ -13,111 +13,111 @@
     </div>
 
     <ul class="weekdays">
-        <li>Lunedì</li>
-        <li>Martedì</li>
-        <li>Mercoledì</li>
-        <li>Giovedì</li>
-        <li>Venerdì</li>
-        <li style="font-weight: bold;">Sabato</li>
-        <li style="font-weight: bold;">Domenica</li>
+        <li>L</li>
+        <li>M</li>
+        <li>M</li>
+        <li>G</li>
+        <li>V</li>
+        <li style="font-weight: bold;">S</li>
+        <li style="font-weight: bold;">D</li>
     </ul>
 
     <ul id="giorni" class="days">  
-        <li id="1"></li>
-        <li id="2"></li>
-        <li id="3">1</li>
-        <li id="4">2</li>
-        <li id="5">3</li>
-        <li id="6">4</li>
-        <li id="7">5</li>
+        <li id="1" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="2" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="3" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">1</li>
+        <li id="4" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">2</li>
+        <li id="5" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">3</li>
+        <li id="6" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">4</li>
+        <li id="7" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">5</li>
 
-        <li id="101"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="102"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="103"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="104"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="105"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="106"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="107"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
+        <li id="101" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="102" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="103" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="104" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="105" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="106" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="107" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
 
-        <li id="8">6</li>
-        <li id="9">7</li>
-        <li id="10">8</li>
-        <li id="11">9</li>
-        <li id="12">10</li>
-        <li id="13">11</li>
-        <li id="14">12</li>
+        <li id="8" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">6</li>
+        <li id="9" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">7</li>
+        <li id="10" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">8</li>
+        <li id="11" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">9</li>
+        <li id="12" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">10</li>
+        <li id="13" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">11</li>
+        <li id="14" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">12</li>
 
-        <li id="108"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="109"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="110"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="111"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="112"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="113"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="114"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
+        <li id="108" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="109" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="110" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="111" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="112" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="113" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="114" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
 
-        <li id="15">13</li>
-        <li id="16">14</li>
-        <li id="17">15</li>
-        <li id="18">16</li>
-        <li id="19">17</li>
-        <li id="20">18</li>
-        <li id="21">19</li>
+        <li id="15" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">13</li>
+        <li id="16" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">14</li>
+        <li id="17" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">15</li>
+        <li id="18" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">16</li>
+        <li id="19" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">17</li>
+        <li id="20" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">18</li>
+        <li id="21" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">19</li>
 
-        <li id="115"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="116"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="117"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="118"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="119"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="120"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="121"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
+        <li id="115" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="116" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="117" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="118" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="119" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="120" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="121" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
 
-        <li id="22">20</li>
-        <li id="23">21</li>
-        <li id="24">22</li>
-        <li id="25">23</li>
-        <li id="26">24</li>
-        <li id="27">25</li>
-        <li id="28">26</li>
+        <li id="22" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">20</li>
+        <li id="23" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">21</li>
+        <li id="24" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">22</li>
+        <li id="25" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">23</li>
+        <li id="26" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">24</li>
+        <li id="27" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">25</li>
+        <li id="28" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">26</li>
 
-        <li id="122"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="123"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="124"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="125"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="126"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="127"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="128"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>30.00<br>biglietto ridotto:<br>10.00</div></li>
+        <li id="122" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="123" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="124" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="125" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="126" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="127" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="128" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>30.00<br>Biglietto ridotto:<br>10.00</div></li>
 
-        <li id="29">27</li>
-        <li id="30">28</li>
-        <li id="31">29</li>
-        <li id="32">30</li>
-        <li id="33"></li>
-        <li id="34"></li>
-        <li id="35"></li>
+        <li id="29" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">27</li>
+        <li id="30" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">28</li>
+        <li id="31" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">29</li>
+        <li id="32" style="border-top-left-radius: 12px; border-top-right-radius: 12px;">30</li>
+        <li id="33" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="34" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="35" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
 
-        <li id="129"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="130"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="131"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="132"><div style="font-size: 12px; padding-bottom: 35px;">biglietto adulto:<br>22.50<br>biglietto ridotto:<br>10.00</div></li>
-        <li id="133"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="134"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="135"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="129" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="130" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="131" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="132" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;">Biglietto adulto:<br>22.50<br>Biglietto ridotto:<br>10.00</div></li>
+        <li id="133" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="134" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="135" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
 
-        <li id="36"></li>
-        <li id="37"></li>
-        <li id="38"></li>
-        <li id="39"></li>
-        <li id="40"></li>
-        <li id="41"></li>
-        <li id="42"></li>
+        <li id="36" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="37" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="38" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="39" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="40" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="41" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
+        <li id="42" style="border-top-left-radius: 12px; border-top-right-radius: 12px;"></li>
 
-        <li id="136"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="137"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="138"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="139"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="140"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="141"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
-        <li id="142"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="136" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="137" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="138" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="139" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="140" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="141" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
+        <li id="142" style="border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;"><div style="font-size: 12px; padding-bottom: 35px;"></div></li>
 
 
     </ul>
