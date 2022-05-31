fa3ad2f2124fe7e2978ffb6f53f85e130d08810c
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-31 13:46:46 +0200

    cookie

--- pages/shop/pagamento.php
+++ pages/shop/pagamento.php
@@ -13,47 +13,23 @@ $tot=htmlspecialchars($_COOKIE['tot']);
 
 
 <?php
-    /*if(!isset($_POST["signupButton"])) {
-        header("Location: index.html");
+    $dbconn = pg_connect("host=postgresql port=5432
+    dbname=lunappark user=extreemedev password=example");
+    session_start();
+    for($i = 0; $i < intval($tInt); $i++) {
+        $email=$_SESSION["id"];
+        $tipo="intero";
+        $query = 'INSERT into public."BIGLIETTO" values ($1,$2,$3,$4)';
+        $result = pg_query_params($dbconn, $query, array($email, $dat, $tipo, $pInt));
     }
-    else{*/
-        $dbconn = pg_connect("host=postgresql port=5432
-        dbname=lunappark user=extreemedev password=example");
-        session_start();
-        for($i = 0; $i < intval($tInt); $i++) {
-            $codice_random = bin2hex(openssl_random_pseudo_bytes(6));
-            $codice_univoco = uniqid().$codice_random;
-            $email=$_SESSION["id"];
-            $tipo="intero";
-            $query = 'INSERT into public."BIGLIETTO" values ($1,$2,$3,$4)';
-            echo (strtotime($dat));
-            $result = pg_query_params($dbconn, $query, array($email, $dat, $tipo, $pInt));
-            
-        }
 
-        for($i = 0; $i < intval($tRid); $i++) {
-            $codice_random = bin2hex(openssl_random_pseudo_bytes(6));
-            $codice_univoco = uniqid().$codice_random;
-            $email=$_SESSION["id"];
-            $tipo="ridotto";
-            $query = 'INSERT into public."BIGLIETTO" values ($1,$2,$3,$4)';
-            $result = pg_query_params($dbconn, $query, array($email, $dat, $tipo, $pRid));
-            
-        }
-        if($result) {
-            /*echo'<script type="title/javascript">myLoader();</script>';*/
-            header("Location: index.php");
-        } else die("C'é stato un errore");
-
-        /*$email=$_SESSION["id"];
-        $dbconn = pg_connect("host=postgresql port=5432
-        dbname=lunappark user=extreemedev password=example");
-        $query = 'INSERT into public."UTENTE" values ($1,$2,$3,$4,$5,$6)';
-        $result = pg_query_params($dbconn, $query, array($email, $nome, $cognome, $password, $telefono, $datanasc));
-        if($result) {*/
-            /*echo'<script type="title/javascript">myLoader();</script>';*/
-            /*header("Location: loading.html");
-        }
-        else die("C'é stato un errore");*/
-   /* }*/
+    for($i = 0; $i < intval($tRid); $i++) {
+        $email=$_SESSION["id"];
+        $tipo="ridotto";
+        $query = 'INSERT into public."BIGLIETTO" values ($1,$2,$3,$4)';
+        $result = pg_query_params($dbconn, $query, array($email, $dat, $tipo, $pRid));    
+    }
+    if($result) {
+        header("Location: index.php");
+    } else die("C'é stato un errore");
 ?>
--- pages/shop/salvaShop.js
+++ pages/shop/salvaShop.js
@@ -1,14 +1,20 @@
 function salvaShop() {
+
+    var now = new Date();
+    var time = now.getTime();
+    time += 3600 * 250; // aggiungo 15 minuti
+    now.setTime(time);
+
     var dat=$("#dataScelta").text();
-    document.cookie="dataB="+dat;
+    document.cookie="dataB="+dat+"; expires="+now.toGMTString();
     var z=parseInt($("#prezzoNormale").text());
-    document.cookie="prezzoInt="+z;
+    document.cookie="prezzoInt="+z+"; expires="+now.toGMTString();
     var x=parseInt($("#bigliettiInt").text());
-    document.cookie="totInt="+x;
+    document.cookie="totInt="+x+"; expires="+now.toGMTString();
     var r=parseInt($("#prezzoRidotto").text());
-    document.cookie="prezzoRid="+r;
+    document.cookie="prezzoRid="+r+"; expires="+now.toGMTString();
     var t=parseInt($("#bigliettiRid").text());
-    document.cookie="totRid="+t;
+    document.cookie="totRid="+t+"; expires="+now.toGMTString();
     var y=parseInt($("#totale").text());
-    document.cookie="tot="+y;
+    document.cookie="tot="+y+"; expires="+now.toGMTString();
 }
