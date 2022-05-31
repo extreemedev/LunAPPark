c3df40757a92fa51658175f239e22a06ecf6f61a
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-31 12:52:41 +0200

    calendario

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -417,11 +417,20 @@ $("#giorni >").click(function(){
     if($(this).css("background-color")=="rgb(255, 182, 86)"){
         /*$("#meseScelto").text($("#mese").text().replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));*/
         /*window.alert("ciao");*/
+
+        for(var c = 0; c<month_names.length; c++){
+            if($("#mese").text().replace(/\s+/g, " ").replace(/^\s|\s$/g, "")==month_names[c]){
+                var m2=c+1;
+                break;
+            }
+        }
+
+
         if(parseInt($(this).attr("id"))<100){
-            $("#dataScelta").text("Giorno : "+$(this).text()+"/"+$("#mese").text().replace(/\s+/g, " ").replace(/^\s|\s$/g, "")+"/"+parseInt($("#anno").text()));
+            $("#dataScelta").text($(this).text()+"/"+m2+"/"+parseInt($("#anno").text()));
         } else{
             var ccc=parseInt($(this).attr("id"))-100;
-            $("#dataScelta").text("Giorno : "+$("#"+ccc).text()+"/"+$("#mese").text().replace(/\s+/g, " ").replace(/^\s|\s$/g, "")+"/"+parseInt($("#anno").text()));
+            $("#dataScelta").text($("#"+ccc).text()+"/"+m2+"/"+parseInt($("#anno").text()));
         }
         var cc=parseInt($(this).attr("id"));
         if(cc==106 || cc==107 || cc==113 || cc==114 || cc==120 || cc==121 || cc==127 || cc==128 || cc==134 || cc==135 || cc==141 || cc==142 || cc==6 || cc==7 || cc==13 || cc==14 || cc==20 || cc==21 || cc==27 || cc==28 || cc==34 || cc==35 || cc==41 || cc==42){
