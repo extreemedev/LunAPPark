1b7e0e8922c3ce79d62996b754c69f70881c7e52
Flayt <64700960+Flayt@users.noreply.github.com>
2022-05-31 12:36:36 +0200

    pagamento

--- pages/shop/pagamento.php
+++ pages/shop/pagamento.php
@@ -1,9 +1,12 @@
 <?php
 $dat=htmlspecialchars($_COOKIE['dataB']);
+
 $pInt=htmlspecialchars($_COOKIE['prezzoInt']);
 $tInt=htmlspecialchars($_COOKIE['totInt']);
+
 $pRid=htmlspecialchars($_COOKIE['prezzoRid']);
 $tRid=htmlspecialchars($_COOKIE['totRid']);
+
 $tot=htmlspecialchars($_COOKIE['tot']);
 ?>
 
@@ -14,15 +17,42 @@ $tot=htmlspecialchars($_COOKIE['tot']);
         header("Location: index.html");
     }
     else{*/
-        $email=$_SESSION["id"];
+        $dbconn = pg_connect("host=postgresql port=5432
+        dbname=lunappark user=extreemedev password=example");
+        session_start();
+        for($i = 0; $i < count($tInt); $i++) {
+            $codice_random = bin2hex(openssl_random_pseudo_bytes(6));
+            $codice_univoco = uniqid().$codice_random;
+            $email=$_SESSION["id"];
+            $tipo="intero";
+            $query = 'INSERT into public."BIGLIETTO" values ($1,$2,$3,$4,$5)';
+            $result = pg_query_params($dbconn, $query, array($codice_univoco, $email, $dat, $tipo, $pInt));
+            
+        }
+
+        for($i = 0; $i < count($tRid); $i++) {
+            $codice_random = bin2hex(openssl_random_pseudo_bytes(6));
+            $codice_univoco = uniqid().$codice_random;
+            $email=$_SESSION["id"];
+            $tipo="ridotto";
+            $query = 'INSERT into public."BIGLIETTO" values ($1,$2,$3,$4,$5)';
+            $result = pg_query_params($dbconn, $query, array($codice_univoco, $email, $dat, $tipo, $pRid));
+            
+        }
+        if($result) {
+            /*echo'<script type="title/javascript">myLoader();</script>';*/
+            header("Location: index.php");
+        } else die("C'é stato un errore");
+
+        /*$email=$_SESSION["id"];
         $dbconn = pg_connect("host=postgresql port=5432
         dbname=lunappark user=extreemedev password=example");
         $query = 'INSERT into public."UTENTE" values ($1,$2,$3,$4,$5,$6)';
         $result = pg_query_params($dbconn, $query, array($email, $nome, $cognome, $password, $telefono, $datanasc));
-        if($result) {
+        if($result) {*/
             /*echo'<script type="title/javascript">myLoader();</script>';*/
-            header("Location: loading.html");
+            /*header("Location: loading.html");
         }
-        else die("C'é stato un errore");
+        else die("C'é stato un errore");*/
    /* }*/
 ?>
