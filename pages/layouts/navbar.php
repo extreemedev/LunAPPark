e906e44916a0c45608216618a37991f19b9d71d0
extreemedev <mattallev@gmail.com>
2022-06-01 14:51:22 +0200

    lasta update

--- pages/layouts/navbar.php
+++ pages/layouts/navbar.php
@@ -7,7 +7,7 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="nav__links navbar-nav ms-auto">
                     <li class="nav-item" style="<?php if(TITLE=="Registrazione"){echo "display:none;";}?>"><a class="nav-link <?php if(TITLE=="LunAPPark"){echo "border-bottom border-warning";}?>" href="/index.php">Scopri il parco</a></li>
-                    <li class="nav-item" style="<?php if(TITLE=="Registrazione"){echo "display:none;";}?>"><a class="nav-link <?php if(TITLE=="Calendario e tariffe"){echo "border-bottom border-warning";}?>" href="/pages/calendar/index.php">Calendario e tariffe + hotel</a></li>
+                    <li class="nav-item" style="<?php if(TITLE=="Registrazione"){echo "display:none;";}?>"><a class="nav-link <?php if(TITLE=="Calendario e tariffe"){echo "border-bottom border-warning";}?>" href="/pages/calendar/index.php">Calendario e tariffe</a></li>
                     <li class="nav-item" style="<?php if(TITLE=="Registrazione"){echo "display:none;";}?>"><a class="nav-link <?php if(TITLE=="Info e contatti"){echo "border-bottom border-warning";}?>" href="/pages/info/index.php">Info e contatti</a></li>
                     <li class="nav-item" style="<?php if(TITLE=="Registrazione"){echo "display:none;";}?>"><a class="nav-link <?php if(TITLE=="Area personale" or TITLE=="Registrazione" or TITLE=="Accedi"){echo "border-bottom border-warning";}?>" href="/pages/login/index.php">Area personale</a></li>
                     <li id="indietro" class="nav-item" style="<?php if(TITLE!="Registrazione"){echo "display:none;";}?>" ><a class="nav-link" href="/pages/login/index.php">Indietro</a></li>
